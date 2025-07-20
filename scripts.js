1e2eebd450fc87d99a345b8bf32d6ec0d57666bb
GuilhermeAyala <134463225+GuilhermeAyala@users.noreply.github.com>
2025-07-20 02:26:55 -0300

    Adição do 'id' na classe Books

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
 class Books {
-    constructor(name, autor, year, price, isAvailable, quantidade){
+    constructor(id, name, autor, year, price, isAvailable, quantidade){
+        this.id = id;
         this.name = name;
         this.autor = autor;
         this.year = year;
@@ -14,14 +15,14 @@ class Books {
 
 }
 
-let Book1 = new Books("Crime e Castigo", "Dostoievsky", 1886, 50.00, true, 2);
-let Book2 = new Books("Dom Casmurro", "Machado de Assis", 1800, 32.50, true, 2);
-let Book3 = new Books("Os miseráveis", "Victor Hugo", 1862, 45.00, true, 1);
+let Book1 = new Books(0, "Crime e Castigo", "Dostoievsky", 1886, 50.00, true, 2);
+let Book2 = new Books(1, "Dom Casmurro", "Machado de Assis", 1800, 32.50, true, 2);
+let Book3 = new Books(2, "Os miseráveis", "Victor Hugo", 1862, 45.00, true, 1);
 
 let books = [Book1, Book2, Book3]
 
 function AdicionarLivro(){
-    books.push(new Books("Timtim", "bkabka", 1289, 30.00, true, 1));
+    books.push(new Books(3, "Hamlet", "William Shakespeare", 1623, 42.00, true, 1));
     console.log("Livro adicionado com sucesso!");
 }
 
