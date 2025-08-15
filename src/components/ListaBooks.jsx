c90171404b287d1e8679cb54b279b0eceb1f024e
GuilhermeAyala <134463225+GuilhermeAyala@users.noreply.github.com>
2025-08-15 00:53:08 -0300

    Estilização jsx
    Estilização parcial do componente ListaBooks.jsx

--- src/components/ListaBooks.jsx
+++ src/components/ListaBooks.jsx
@@ -7,7 +7,7 @@ function ListaBooks() {
       <h2>Livros Disponíveis:</h2>
       <ul>
         {books.map((book) => (
-          <li key={book.id}>
+          <li key={book.id} style={ {width: 250, height: 220, border: 2, borderStyle: "solid", borderRadius: 10, padding: 5, alignItems: "center"} }>
             <h4>Titulo: {book.name}</h4>  
             <h5>Autor: {book.autor}</h5>
             <h5>Preço: R${book.price}</h5>
