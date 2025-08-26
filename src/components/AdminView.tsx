706680012e29345925b108967e20d688dd045fba
GuilhermeAyala <134463225+GuilhermeAyala@users.noreply.github.com>
2025-08-25 23:07:51 -0300

    Estilização do AdminView
    Além da estilização teve o começo na tentativa de adicionar livros os transformando em arquivo json

--- src/components/AdminView.tsx
+++ src/components/AdminView.tsx
@@ -1,6 +1,8 @@
 import React, {useState} from "react";
+import { Books } from "../data/books";
 //import { Admin } from "../data/adm"; 
 
+let book = new Books();
 function AdminInterface(){
    
     let [count, setCount] = useState(0);
@@ -19,16 +21,18 @@ function AdminInterface(){
         return count;
     }
 
-    const AddLivroAoSistema = () => {}
+    const AddLivroAoSistema = () => {
+        const ObjectBook = JSON.stringify({book: [new Books()]})
+    }
 
     return(//pd virar json pro BD, além da parte react 
         <div>
             <h3>Gerenciar Livros</h3>
             <h3>Estoque</h3>
-            <input type="text" value={produto} placeholder="Nome Produto"></input>
-            <input type="text" value={autor} placeholder="Autor"></input>
-            <input type="number" value={ano} placeholder="Ano publicação"></input>
-            <input type="number" value={price} placeholder="Preço"></input><br />
+            <input type="text" value={produto} placeholder="Nome Produto" style={{height: 30, borderRadius: 15}}></input>
+            <input type="text" value={autor} placeholder="Autor" style={{height: 30, borderRadius: 15}}></input>
+            <input type="number" value={ano} placeholder="Ano publicação" style={{height: 30, borderRadius: 15}}></input>
+            <input type="number" value={price} placeholder="Preço" style={{height: 30, borderRadius: 15}}></input><br />
             <button onClick={IncrementQtd}>+</button>Quantidade: {count}<button onClick={DecrementQtd}>-</button><br />
             <button onClick={AddLivroAoSistema} 
             style={{width: 150, height: 60, borderRadius: 15, backgroundColor: "red", color: "white"}}>Adicionar Livro ao sistema</button>
--- src/components/routes.jsx
+++ src/components/routes.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { Route, BrowserRouter} from 'react-router-dom';
 import Home from "./Home";
 import CarrinhoView from "./Carrinho";
+import AdminInterface from "./AdminView";
 
 const Routes = () => {
     return(
         <BrowserRouter>
             <Route Component = { Home } path="/" />
             <Route Component = { CarrinhoView } path="/Carrinho" />
+            <Route Component={ AdminInterface } path="/Admin" />
         </BrowserRouter>
     )
 }
