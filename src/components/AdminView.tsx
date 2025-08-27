896fd3943756e1e8b5099d64d40a486c6c683fe9
GuilhermeAyala <134463225+GuilhermeAyala@users.noreply.github.com>
2025-08-26 23:15:15 -0300

    Update AdminView.tsx
    Funcionamento do formulário para dição de livros por parte do Admin  e o livro vira json para futura integração de BD

--- src/components/AdminView.tsx
+++ src/components/AdminView.tsx
@@ -2,42 +2,84 @@ import React, {useState} from "react";
 import { Books } from "../data/books";
 //import { Admin } from "../data/adm"; 
 
-let book = new Books();
+type Produto = {
+    produto: string,
+    price: number,
+    autor: string;
+    ano: number;
+    quantidade: number;
+}
+
 function AdminInterface(){
    
     let [count, setCount] = useState(0);
-    let [produto, setProduto] = useState();
-    let [price, setPrice] = useState();
-    let [autor, setAutor] = useState();
-    let [ano, setAno] = useState();
+    let [produto, setProduto] = useState<string>('');
+    let [price, setPrice] = useState<string>('');
+    let [autor, setAutor] = useState<string>('');
+    let [ano, setAno] = useState<string>('');
+    let [quantidade, setQuantidade] = useState<number>(0);
+    let [jsonDados, setJsonDados] = useState<Produto | null>(null);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const novoProduto: Produto = {
+        produto,
+        price: Number(price),
+        autor,
+        ano: Number(ano),
+        quantidade
+    };
+
+    setJsonDados(novoProduto);
+    };
 
     const IncrementQtd = () => {
-        setCount(count + 1)
+        setCount(count + 1);
+        setQuantidade(quantidade + 1);
     }
     const DecrementQtd = () => {
-        setCount(count - 1)
+        setCount(count - 1);
+        setQuantidade(quantidade - 1);
     }
-    if(count < 0){
+    if(count && quantidade < 0){
         return count;
     }
 
-    const AddLivroAoSistema = () => {
+    const AddLivroAoSistema = () => {//n faz nd por enqt, ver a logica depois
         const ObjectBook = JSON.stringify({book: [new Books()]})
+    }
+
+    const AddFuncionarioAoSistema = () => {
+        const ObjectFuncionario = null;// por enqt vazio, ideia: criar um objeto de funcionario para BD
     }
 
     return(//pd virar json pro BD, além da parte react 
         <div>
             <h3>Gerenciar Livros</h3>
             <h3>Estoque</h3>
-            <input type="text" value={produto} placeholder="Nome Produto" style={{height: 30, borderRadius: 15}}></input>
-            <input type="text" value={autor} placeholder="Autor" style={{height: 30, borderRadius: 15}}></input>
-            <input type="number" value={ano} placeholder="Ano publicação" style={{height: 30, borderRadius: 15}}></input>
-            <input type="number" value={price} placeholder="Preço" style={{height: 30, borderRadius: 15}}></input><br />
-            <button onClick={IncrementQtd}>+</button>Quantidade: {count}<button onClick={DecrementQtd}>-</button><br />
-            <button onClick={AddLivroAoSistema} 
-            style={{width: 150, height: 60, borderRadius: 15, backgroundColor: "red", color: "white"}}>Adicionar Livro ao sistema</button>
+            <form onSubmit={handleSubmit}>
+                <input type="text" value={produto} placeholder="Nome Produto" 
+                onChange={(e) => setProduto(e.target.value)} style={{height: 30, borderRadius: 15}}></input>
+
+                <input type="text" value={autor} placeholder="Autor" 
+                onChange={(e) => setAutor(String(e.target.value))} style={{height: 30, borderRadius: 15}}></input>
+
+                <input type="number" value={ano} placeholder="Ano publicação" 
+                onChange={(e) => setAno(String(e.target.value))} style={{height: 30, borderRadius: 15}}></input>
+
+                <input type="number" value={price} placeholder="Preço" 
+                onChange={(e) => setPrice(String(e.target.value))} style={{height: 30, borderRadius: 15}}></input><br />
+
+                <button onClick={IncrementQtd}>+</button>Quantidade: {count}<button onClick={DecrementQtd}>-</button><br />
+                <button onClick={AddLivroAoSistema} 
+                style={{width: 150, height: 60, borderRadius: 15, backgroundColor: "red", color: "white"}}>Adicionar Livro ao sistema</button>
+
+                {jsonDados && (<pre>{JSON.stringify(jsonDados, null, 2)}</pre>)}
+            </form>
 
             <h3>Histórico de Compras</h3>
+
         </div>
     );
 }
