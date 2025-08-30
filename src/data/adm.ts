c393ecb24531286d0bad127a54a9eeeb4e9e8cd5
GuilhermeAyala <134463225+GuilhermeAyala@users.noreply.github.com>
2025-08-29 23:31:46 -0300

    nova feature no adm.ts
    Inicio da funcionalidade para add funcionario e criação da classe

--- src/data/adm.ts
+++ src/data/adm.ts
@@ -13,8 +13,35 @@ export class Admin {
     }
 }
 
+class Funcionarios {
+    id;
+    name;
+    salario; 
+    cargo;
+    idade;
+    totalDeVendas;
+    constructor(id: number, name: string, salario: number, cargo: string, idade: number, totalDeVendas: number){
+        this.id = id;
+        this.name = name;
+        this.salario = salario;
+        this.cargo = cargo;
+        this.idade = idade;
+        this.totalDeVendas = totalDeVendas;
+    }
+}
+
 const Admin1 = new Admin(0, "Peter", "peter@gmail.com", 12345)
 
-const AdicionarFuncionarios = () => {}
+let Funcionario1 = new Funcionarios(0, "Lucas", 1200, "Vendedor", 25, 1500)
+let ArrayFuncionarios = [Funcionario1];
+
+const AdicionarFuncionarios = (...novosFuncionarios) => {
+    novosFuncionarios.forEach(funcionario => {
+        ArrayFuncionarios.push();
+        return ArrayFuncionarios;
+    });
+}
+
+console.log("Hello");
 
 const GerenciarEstoque = () => {}
