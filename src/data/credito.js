69251f8f40266c336a9d55b90842af7feee45756
GuilhermeAyala <134463225+GuilhermeAyala@users.noreply.github.com>
2026-01-03 01:03:28 -0300

    lógica de pagamento
    Lógica de pagamento, visando utilização na view/no jsx

---
+++ src/data/credito.js
@@ -0,0 +1,14 @@
+export class Credito extends Pagamento {
+    constructor(){
+        super(id);
+        this.nomeTitular = this.nomeTitular;
+        this.numeroCartao = this.numeroCartao;
+        this.validade = this.validade;
+        this.marca = this.marca;
+        this.cvc = this.cvc;
+    }
+
+}
+
+    const AdicionarCartão = () => {}
+    const ValidarCartao = () => {}
