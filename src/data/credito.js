bae7e86aa78a99a1984a7932f8dc5807ed5e3dd3
GuilhermeAyala <134463225+GuilhermeAyala@users.noreply.github.com>
2026-02-10 23:05:15 -0300

    Começo arquivo credito.js
    Método de pagamento via cartão crédito criado, validações, regras de negócio e etc

--- src/data/credito.js
+++ src/data/credito.js
@@ -1,3 +1,5 @@
+import { total } from ""
+
 export class Credito extends Pagamento {
     constructor(){
         super(id);
@@ -6,9 +8,19 @@ export class Credito extends Pagamento {
         this.validade = this.validade;
         this.marca = this.marca;
         this.cvc = this.cvc;
+        this.saldo = saldo;
     }
 
 }
 
     const AdicionarCartão = () => {}
     const ValidarCartao = () => {}
+    const VerificarSaldo = () => {
+        if(this.saldo < 0){
+            console.log("Saldo insuficiente");
+        }
+        else{
+            this.saldo - total; //total vem do carrinho/books
+            return new this.saldo;
+        }
+    }
