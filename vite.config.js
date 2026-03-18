125c7fdc0d8087c0a1e3c8b66ccc51dfc27e868f
GuilhermeAyala <134463225+GuilhermeAyala@users.noreply.github.com>
2026-03-17 23:57:33 -0300

    Add Tailwind

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
@@ -6,5 +7,8 @@ export default defineConfig({
   server: {
     port: 3000
   },
-  plugins: [react()],
+  plugins: [
+    tailwindcss(),
+    react()
+  ],
 });
